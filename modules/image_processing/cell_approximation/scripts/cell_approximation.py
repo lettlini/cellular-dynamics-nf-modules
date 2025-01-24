1c965ace77f38281d443a165fa5b60084cfa61d3
Kolya Lettl <klettl@posteo.com>
2025-01-24 16:53:17 +0100

    fix: fixed bug in cell approximation that resulted in fragmented cells

--- modules/image_processing/cell_approximation/scripts/cell_approximation.py
+++ modules/image_processing/cell_approximation/scripts/cell_approximation.py
@@ -15,6 +15,26 @@ class CellApproximationTransformation(BaseDataSetTransformation):
         super().__init__()
 
     @staticmethod
+    def ensure_cell_integrity(cell_mask, nucleus_mask):
+        num_cell_ccs, labelled_ccs, stats, _ = cv2.connectedComponentsWithStats(
+            cell_mask.astype(np.uint8)
+        )
+        set_zero_mask = np.zeros_like(cell_mask, dtype=bool)
+
+        if num_cell_ccs != 2:
+            nucleus_overlapping_ccs = np.setdiff1d(labelled_ccs[nucleus_mask], (0,))
+
+            keep_index = nucleus_overlapping_ccs[
+                np.argmax(stats[nucleus_overlapping_ccs, cv2.CC_STAT_AREA], axis=0)
+            ]
+
+            set_zero_mask[np.logical_and(cell_mask > 0, labelled_ccs != keep_index)] = (
+                True
+            )
+
+        return set_zero_mask
+
+    @staticmethod
     def get_disconnected(label_image: np.array, nuclei_mask: np.array):
 
         all_cell_labels = np.setdiff1d(label_image, (0,))
@@ -34,13 +54,14 @@ class CellApproximationTransformation(BaseDataSetTransformation):
 
             binary_cell_mask = label_image == cell_label
 
-            num_labels, _ = cv2.connectedComponents(binary_cell_mask.astype(np.uint8))
+            # cells are possibly already disconnected, so we remove all but the
+            # largest nucleus-overlapping cc
 
-            if num_labels > 2:
-                # we have more than one connected component, keep only the largest one that has overlap with the
-                # corresponding nucleus
-
-                raise RuntimeError("More than one connected component in cell mask")
+            set_zero_mask = CellApproximationTransformation.ensure_cell_integrity(
+                binary_cell_mask, nuclei_labelled == corresponding_nucleus_label
+            )
+            binary_cell_mask[set_zero_mask] = 0
+            disconnected_cell_labels[set_zero_mask] = 0
 
             contours, _ = cv2.findContours(
                 binary_cell_mask.astype(np.uint8),
@@ -63,29 +84,13 @@ class CellApproximationTransformation(BaseDataSetTransformation):
                     if np.setdiff1d(current_neighbors, (0, cell_label)).size > 0:
                         disconnected_cell_labels[row, column] = 0
 
-            new_num_ccs, new_labelled, new_stats, _ = cv2.connectedComponentsWithStats(
-                (disconnected_cell_labels == cell_label).astype(np.uint8),
-                connectivity=8,
+            # this procedure might have disconnected cells again, so we need to,
+            # again, remove all but the largest nucleus-overlapping cc
+            set_zero_mask = CellApproximationTransformation.ensure_cell_integrity(
+                disconnected_cell_labels == cell_label,
+                nuclei_labelled == corresponding_nucleus_label,
             )
-            if new_num_ccs > 2:
-                # removing pixels lead to two different connected components with the same label
-                # we only keep the one that overlaps with the corresponding nucleus and is the largest
-                overlapping_labels = np.setdiff1d(
-                    new_labelled[nuclei_labelled == corresponding_nucleus_label], (0,)
-                )
-
-                # we wanto keep the largest CC that has overlap with the nucleus
-                keep_index = overlapping_labels[
-                    np.argmax(new_stats[overlapping_labels, cv2.CC_STAT_AREA], axis=0)
-                ]
-
-                # we set all other labels to zero
-                disconnected_cell_labels[
-                    np.logical_and(
-                        disconnected_cell_labels == cell_label,
-                        new_labelled != keep_index,
-                    )
-                ] = 0
+            disconnected_cell_labels[set_zero_mask] = 0
 
         return disconnected_cell_labels
 
@@ -123,13 +128,13 @@ class CellApproximationTransformation(BaseDataSetTransformation):
         # step 4: some post-processing
         dislabels[dislabels == -1] = 0  # set boundaries to 0
 
-        dislabels = CellApproximationTransformation.get_disconnected(
-            dislabels, binary_nuclei_mask
-        )
-
         # limit cell area by distance transform
         if self._cell_cutoff_px is not None:
             dislabels[bg_mask] = 0
+
+        dislabels = CellApproximationTransformation.get_disconnected(
+            dislabels, binary_nuclei_mask
+        )
 
         # we remove the padding
         dislabels = dislabels[1:-1, 1:-1]
