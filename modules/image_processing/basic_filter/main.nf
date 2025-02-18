4f5c68874f60e903bf4d6d3ab14d3b85220966b0
Kolya Lettl <klettl@posteo.com>
2025-02-18 17:44:02 +0100

    refactor: update basic_filter process to use dataset and parent configurations

--- modules/image_processing/basic_filter/main.nf
+++ modules/image_processing/basic_filter/main.nf
@@ -1,27 +1,25 @@
 process basic_filter {
 
-    publishDir "${parent_dir_out}/${basename}", mode: 'copy'
+    publishDir "${publish_dir}/${basename}", mode: 'copy'
 
     label "low_cpu", "short_running"
 
-    conda "${moduleDir}/environment.yml" 
+    conda "${moduleDir}/environment.yml"
 
     input:
-    tuple val(basename), path(dataset_path)
-    val drop_first_n
-    val drop_last_m
-    val parent_dir_out
+    tuple val(basename), path(dataset_config), path(parent_config), path(dataset_path)
+    val publish_dir
 
     output:
-    tuple val(basename), path("confluency_filtered.pickle"), emit: results
+    tuple val(basename), path(dataset_config), path(parent_config), path("confluency_filtered.pickle"), emit: results
 
     script:
     """
 	 python ${moduleDir}/scripts/filter.py \
         --infile="${dataset_path}" \
         --outfile="confluency_filtered.pickle" \
-        --drop_first_n=${drop_first_n} \
-        --drop_last_m=${drop_last_m} \
+        --dataset_config="${dataset_config}" \
+        --parent_config="${parent_config}" \
         --cpus=${task.cpus}
     """
 }
--- modules/image_processing/basic_filter/scripts/filter.py
+++ modules/image_processing/basic_filter/scripts/filter.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 
 from core_data_utils.datasets import BaseDataSet, BaseDataSetEntry
 from core_data_utils.transformations import BaseFilter
+import toml
 
 
 class FirstLastFilter(BaseFilter):
@@ -33,15 +34,15 @@ if __name__ == "__main__":
         "--outfile", required=True, type=str, help="Path to output file"
     )
     parser.add_argument(
-        "--drop_first_n",
+        "--dataset_config",
         required=True,
-        type=int,
+        type=str,
         help="Drop first n entries from DataSet",
     )
     parser.add_argument(
-        "--drop_last_m",
+        "--parent_config",
         required=True,
-        type=int,
+        type=str,
         help="Drop last m entries from DataSet",
     )
     parser.add_argument(
@@ -53,7 +54,14 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    dataset_config = toml.load(args.dataset_config)
+    parent_config = toml.load(args.parent_config)
+    full_config = parent_config | dataset_config
+
     x = BaseDataSet.from_pickle(args.infile)
-    x = FirstLastFilter(first_n=args.drop_first_n, last_m=args.drop_last_m)(x)
+    x = FirstLastFilter(
+        first_n=full_config["data-preparation"]["drop_first_n"],
+        last_m=full_config["data-preparation"]["drop_last_m"],
+    )(x)
 
     x.to_pickle(args.outfile)
